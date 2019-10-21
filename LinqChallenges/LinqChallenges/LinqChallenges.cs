87543e9fa9ec41a010dbff8d15542ed9b533963d
alexHardy872 <alexhardy872@gmail.com>
2019-10-21 10:05:18 -0500

    first problem solved, substring filter, passes test

---
+++ LinqChallenges/LinqChallenges/LinqChallenges.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqChallenges
+{
+    public static class LinqChallenge
+    {
+
+        public static List<string> FilterForSubString (List<string> words, string subCheck)
+        {
+
+
+            var filteredWords =  words.Where(w => w.Contains(subCheck)).ToList();
+            return filteredWords;
+          
+        }
+
+
+        
+        // go through list
+        // filter for substring "th" list.Contains
+
+
+
+
+    }
+}
--- LinqChallenges/LinqChallengesTest/LinqChallengesTest.cs
+++ LinqChallenges/LinqChallengesTest/LinqChallengesTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LinqChallenges;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LinqChallengesTest
 {
@@ -7,8 +10,32 @@ namespace LinqChallengesTest
   public class LinqChallengesTest
   {
     [TestMethod]
-    public void TestMethod1()
+    public void FilterForSubString_findwordswithTH_CheckList()
     {
+
+        List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+        string substring = "th";
+        List<string> expected = new List<string>() { "the", "this", "tenth", "mathematics" };
+
+            // arrange
+            // act
+        List<string> actual = LinqChallenge.FilterForSubString(words, substring);
+            // assert
+
+            CollectionAssert.AreEqual(expected, actual);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
     }
-  }
 }
