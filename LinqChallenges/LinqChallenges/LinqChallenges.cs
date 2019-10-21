53cf43e25eee2a8c6dca2058190c9df648650e96
alexHardy872 <alexhardy872@gmail.com>
2019-10-21 13:09:06 -0500

    manual work around for problem 3 NOT using linq, got test working, needs to be refactored in Linq

--- LinqChallenges/LinqChallenges/LinqChallenges.cs
+++ LinqChallenges/LinqChallenges/LinqChallenges.cs
@@ -19,6 +19,40 @@ namespace LinqChallenges
           return names.Distinct().ToList();
         }
 
+        public static double AverageGrades(List<string> gradeStrings)
+        {
+
+            // average
+            // min/ max (rid lowesr)
+
+
+            //gradeStrings.Select(s => s.Split(',').Select(g => Double.Parse(g)));
+
+            List<double> numberGrades = new List<double>();
+            foreach(string set in gradeStrings)
+            {
+                string[] grades = set.Split(',');
+
+                grades.ToList();
+
+                List<double> numbers = new List<double>();
+
+                foreach (string numString in grades)
+                {
+                    numbers.Add(Double.Parse(numString));
+                }
+                numbers.Remove(numbers.Min());
+                numberGrades.Add(numbers.Average());
+                
+                
+                
+            }
+
+
+
+            return numberGrades.Average();
+
+        }
 
 
 
--- LinqChallenges/LinqChallengesTest/LinqChallengesTest.cs
+++ LinqChallenges/LinqChallengesTest/LinqChallengesTest.cs
@@ -38,10 +38,28 @@ namespace LinqChallengesTest
       CollectionAssert.AreEqual(expected, actual);
     }
 
+    [TestMethod]
+    public void AverageGrades_testAverageGrades_returnsexpected()
+    {
+            // arrange
+            List<string> classGrades = new List<string>() {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55" };
+            double expected = 86.125;
+
+        // act
+            double actual = LinqChallenge.AverageGrades(classGrades);
+
+        // assert
+        Assert.AreEqual(expected, actual);
+    }
 
 
 
 
 
-  }
+
+    }
 }
